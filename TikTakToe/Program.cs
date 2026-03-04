383262db7fce23f099d73a4121ad919ac9115481
yudai-yam <yudaimadeinjapan@gmail.com>
2026-03-04 16:01:53 +0100

    restructure the whole project for adding test folder

---
+++ TikTakToe/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace TikTakToe;
+using TikTakToe.UI;
+using TikTakToe.Game;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Game started");
+
+        // start the Game 
+        IGameUI ui = new ConsoleUI();
+
+        GameController gameController = new GameController(ui);
+        gameController.StartGame();
+
+    }
+}
