1cd9184ac1fe95cf92719063b2d97373413a4ed4
yudai-yam <yudaimadeinjapan@gmail.com>
2026-03-10 16:45:39 +0100

    gemacontrollertests

--- TikTakToe.Tests/Game/GameControllerTests.cs
+++ TikTakToe.Tests/Game/GameControllerTests.cs
@@ -1,5 +1,6 @@
 using TikTakToe.Game;
-
+using Moq;
+using TikTakToe.UI;
 namespace TikTakToe.Tests.Game;
 
 public class GameControllerTests
@@ -19,12 +20,18 @@ public class GameControllerTests
         inputSequence.Enqueue((1, 1)); // Player O
         inputSequence.Enqueue((0, 2)); // Player X wins
 
+        // setup the mock for the GetPlayerMove()
+        // execute the lamda function every time GetPlayerMove() is called to return the next input from the queue
+        // if .Returns(inputSequence.Dequeue(), it would execute the Dequeue() only the first time
+        // it would return (0,0) for all subsequent calls
         mockUI.Setup(ui => ui.GetPlayerMove()).Returns(() => inputSequence.Dequeue());
 
         // Act
         gameController.StartGame();
 
         // Assert
+        // Times.Once means exactly 1 time
+        // verify that ShowMessage was called with "Player X wins!" exactly once
         mockUI.Verify(ui => ui.ShowMessage("Player X wins!"), Times.Once);
     } 
 }
