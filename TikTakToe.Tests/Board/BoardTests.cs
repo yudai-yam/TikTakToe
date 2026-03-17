b87ede0100c12fbec78a586e67018a6885f930d0
yudai-yam <yudaimadeinjapan@gmail.com>
2026-03-17 14:36:58 +0100

    add borad test

--- TikTakToe.Tests/Board/BoardTests.cs
+++ TikTakToe.Tests/Board/BoardTests.cs
@@ -1,15 +1,26 @@
+using Newtonsoft.Json.Converters;
 using TikTakToe.Board;
 
 namespace TikTakToe.Tests.Board;
 
 public class BoardTests
 {
-    [Fact]
-    public void UpdateBoard_Should_Update_Cell_State()
+    // Define a data source
+    public static IEnumerable<object[]> TestData()
+    {
+        yield return new object[] { (row: 1, column: 1) };
+        yield return new object[] { (row: 0, column: 0) };
+        yield return new object[] { (row: 2, column: 2) };
+        yield return new object[] { (row: 1, column: 0) };
+        yield return new object[] { (row: 0, column: 1) };
+    }
+
+    [Theory]
+    [MemberData(nameof(TestData))]
+    public void UpdateBoard_Should_Update_Cell_State((int row, int column) move)
     {
         // Arrange
         var board = new TikTakToe.Board.Board();
-        var move = (row: 1, column: 1);
         var player = CellState.X;
 
         // Act
@@ -33,4 +44,60 @@ public class BoardTests
         // Act & Assert
         Assert.Throws<InvalidOperationException>(() => board.UpdateBoard(move, player2));
     }   
+
+    [Fact]
+    public void HasWnnter_Should_Return_True_When_There_Is_A_Winner()
+    {
+        // arrange
+        var board = new TikTakToe.Board.Board();
+        board.UpdateBoard((0, 0), CellState.X);
+        board.UpdateBoard((0, 1), CellState.X);
+        board.UpdateBoard((0, 2), CellState.X);
+
+        // act
+        var hasWinner = board.HasWinner();
+
+        // assert
+        Assert.True(hasWinner);
+
+    }
+
+    [Fact]
+    public void HasWnnter_Should_Return_False_When_There_Is_No_Winner()
+    {
+        // arrange
+        var board = new TikTakToe.Board.Board();
+        board.UpdateBoard((0, 0), CellState.X);
+        board.UpdateBoard((0, 1), CellState.X);
+        board.UpdateBoard((1, 2), CellState.X);
+
+        // act
+        var hasWinner = board.HasWinner();
+
+        // assert
+        Assert.False(hasWinner);
+
+    }
+
+    [Fact]
+    public void IsFull_Should_Return_True_When_Board_Is_Full()
+    {
+        // arrange
+        var board = new TikTakToe.Board.Board();
+        board.UpdateBoard((0, 0), CellState.X);
+        board.UpdateBoard((0, 1), CellState.X);
+        board.UpdateBoard((0, 2), CellState.X);
+        board.UpdateBoard((1, 0), CellState.X);
+        board.UpdateBoard((1, 1), CellState.X);
+        board.UpdateBoard((1, 2), CellState.X);
+        board.UpdateBoard((2, 0), CellState.X);
+        board.UpdateBoard((2, 1), CellState.X);
+        board.UpdateBoard((2, 2), CellState.X);
+
+        // act
+        var isFull = board.IsFull();
+
+        // assert
+        Assert.True(isFull);
+    }
 }
